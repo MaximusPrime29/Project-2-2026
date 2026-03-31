78e146dace29502350bbc9a6d68342c7939d10d0
Max Schulte <142296959+MaximusPrime29@users.noreply.github.com>
2026-03-31 16:00:40 +0200

    Working drag and drop

---
+++ Assets/Scripts/PlantDrag.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantDrag : MonoBehaviour
+{
+
+    private Vector3 offset;
+    private bool dragging = false;
+
+
+    
+    private void OnMouseDown()
+    {
+        Debug.Log("in mousedown");
+        offset= transform.position -Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        dragging = true;
+
+    }
+    private void OnMouseDrag()
+    {
+
+        if (dragging)
+        {
+
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.z = 0;
+            transform.position = mousePos + offset;
+
+        }
+        
+    }
+    private void OnMouseUp()
+    {
+        dragging = false;
+    }
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+
+    
+
+}
