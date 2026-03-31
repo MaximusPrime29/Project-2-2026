498dfd50c88147038850e5905d9b98899edb9445
Max Schulte <142296959+MaximusPrime29@users.noreply.github.com>
2026-03-31 16:11:12 +0200

    Colour feedback based on zone placement

--- Assets/Scripts/PlantDrag.cs
+++ Assets/Scripts/PlantDrag.cs
@@ -8,8 +8,11 @@ public class PlantDrag : MonoBehaviour
     private Vector3 offset;
     private bool dragging = false;
 
+    public string currentZone = "None";
+    public string lightRequirement = "Medium";
 
-    
+
+
     private void OnMouseDown()
     {
         Debug.Log("in mousedown");
@@ -22,11 +25,9 @@ public class PlantDrag : MonoBehaviour
 
         if (dragging)
         {
-
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             mousePos.z = 0;
             transform.position = mousePos + offset;
-
         }
         
     }
@@ -35,15 +36,45 @@ public class PlantDrag : MonoBehaviour
         dragging = false;
     }
 
-    void Start()
-    {
-        
-    }
+   
     void Update()
     {
-        
+        if (currentZone == lightRequirement)
+        {
+            GetComponent<SpriteRenderer>().color = Color.green;
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().color = Color.red;
+        }
     }
 
-    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("LowLight"))
+        {
+            currentZone = "Low";
+        }
+        else if (other.CompareTag("MediumLight"))
+        {
+            currentZone = "Medium";
+        }
+
+        else if (other.CompareTag("HighLight"))
+        {
+            currentZone = "High";
+        }
+
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("LowLight") || other.CompareTag("MediumLight") || other.CompareTag("HighLight"))
+        {
+            currentZone = "None";
+        } 
+
+    }
+
+
 
 }
